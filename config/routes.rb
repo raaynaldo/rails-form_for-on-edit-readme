ac1bb754701692ac77934c96c7d36d5393130d66
raaynaldo <raaynaldo@hotmail.com>
2020-10-31 17:45:56 -0400

    commit tree d930d02e94b1bf08345474040f3753ae7372474f

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "/posts", to: "posts#index"
+  # get "/posts/new", to: "posts#new", as: "new_post"
+  # get "/posts/:id", to: "posts#show", as: "post"
+  # post "/posts", to: "posts#create"
+  # get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
+  # patch "/posts/:id", to: "posts#update"
+  resources :posts, only: [:index, :show, :new, :create, :edit]
+  patch 'posts/:id', to: 'posts#update'
 end
